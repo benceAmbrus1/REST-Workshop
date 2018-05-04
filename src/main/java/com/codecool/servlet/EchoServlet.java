0cd2dec61b067cc97d96041d01f16ad4c9c3f59d
Domokos Tibor <Domokos.Tibor.82@gmail.com>
2018-05-04 11:36:07 +0200

    echo

--- src/main/java/com/codecool/servlet/EchoServlet.java
+++ src/main/java/com/codecool/servlet/EchoServlet.java
@@ -1,13 +1,25 @@
 package com.codecool.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.sql.rowset.serial.SerialException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("/echo")
 public class EchoServlet extends HttpServlet {
 
-    protected void doGet(HttpServlet req, HttpServlet resp) throws SerialException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String message = req.getParameter("message");
+
+        resp.getWriter().write(message);
+        resp.getWriter().flush();
+        resp.getWriter().close();
+
+//        req.setAttribute("message", message);
+        resp.setStatus(200);
 
     }
 
